71313ed1fc3615894d74ca38e751268fdb5a97b0
Mshimaz <shimaz.abc.ms@gmail.com>
2025-12-11 19:06:46 +0530

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shimaz-portfolio/', // MATCH YOUR REPO NAME
+  base: '/portfolio/', // MATCH YOUR REPO NAME
 })
